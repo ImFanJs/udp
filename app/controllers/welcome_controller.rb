ee9d95bc9b376bb08eb28d1e2db6d96cee622f0e
Jesus Castaneda <jesco.marketing@gmail.com>
2016-02-02 10:00:53 -0600

    Produccion

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,25 @@
+class WelcomeController < ApplicationController
+  def index
+  	@title = "Inicio | UDP"
+  end
+
+  def quienes_somos
+  	@title = "Quienes Somos | UDP"
+  end
+
+  def carreras
+  	@title = "Carreras | UDP"
+  end
+
+  def servicios
+    @title = "Servicios | UDP"
+  end
+
+  def contacto
+    @title = "Contacto | UDP"
+  end
+
+  def correo
+  	@title = "Gracias"
+  end
+end
